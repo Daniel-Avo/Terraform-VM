1308381836e9309abaaf849026c18a04c807db82
Daniel <Daniel@DESKTOP-7IMDU22>
2022-01-05 14:09:48 +0100

    adding resource

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
+
+resource "aws_instance" "web" {
+  ami           = ami-0e472ba40eb589f49
+  instance_type = "t2.micro"
+
+  }
